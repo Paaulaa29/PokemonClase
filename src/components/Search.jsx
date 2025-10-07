7923606e328c1d4a8047b39f74702e644a8f5e65
Paaulaa29 <alumno.647654@ies-azarquiel.es>
2025-10-07 11:41:49 +0200

    Errores

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
 
 
 const Search = ({ onSearch }) => { //Recibe una función como parámetro
- const handleInputChange = (e) => { //
-   onSearch(e.target.value);    //
- };
-   return (
-       <input
-           type="search"
-           placeholder="Buscar..."
-           onChange={handleInputChange} //Cada vez que se teclea se llama al onSearch pasándole la query
-       />
-   );
+    const handleInputChange = (e) => { //
+        onSearch(e.target.value);    //
+    };
+    return (
+        <div className="d-flex align-items-center border rounded-pill px-3 py-1">
+            <span className="iconStyle">
+                <FaSearch />
+            </span>
+            <input
+                type="search"
+                placeholder="Buscar..."
+                onChange={handleInputChange} //Cada vez que se teclea se llama al onSearch pasándole la query
+                className="inputStyle"
+            />
+        </div>
+    );
 };
 export default Search;
